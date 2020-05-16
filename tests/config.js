ee0f926270465c7a465771bdf4de46d64936116a
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2020-05-16 18:48:47 -0300

    Created test integration and added new comands scripts

--- tests/config.js
+++ tests/config.js
@@ -1,8 +1,11 @@
 import chai from "chai";
 import sinon from "sinon";
+import supertest from "supertest";
 import "../src/config/LoaderEnvironmentVariable";
+import app from "../src/config/Server"
 
 
 global.expect = chai.expect;
 global.sinon = sinon;
 global.assert = chai.assert
+global.request = supertest(app);
---
+++ tests/integration/Search.spec.js
@@ -0,0 +1,31 @@
+
+describe("Tests Integration of /searchs endpoint", () => {
+
+    it("Should return status code 400 to the try request without passed datas needed", function (done) {
+        request.post("/searchs").expect(400).end(done);
+    });
+
+    it("Should return status code 200 to the try request with datas needed", function(done) {
+        const bodyRequestFake = {
+            search: "celular",
+            limit: 1
+        }
+        request.post("/searchs").send(bodyRequestFake).expect(200).end(done);
+    });
+
+    it("Should return quantity products equal parameter limit", function(done) {
+        const bodyRequestFake = {
+            search: "celular",
+            limit: 1
+        }
+        request.post("/searchs")
+            .send(bodyRequestFake)
+            .expect(200)
+            .then((response) => { 
+                const quantityProducts = response.body.length;
+                expect(bodyRequestFake.limit).to.be.equal(quantityProducts);
+                done();
+            });
+    });
+
+});
