f2dc10430a6baf8d0975991af1d38b4c0da2fb0c
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-03-14 02:00:09 -0300

    Committed project and adding kubernetes

--- src/config/Cache.js
+++ src/config/Cache.js
@@ -1,7 +1,10 @@
 import redis from "redis";
 import logger from "./Logger";
 
-const client = redis.createClient();
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
 
 client.on("error", (error) => {
     logger.error(`${error.name} caused by: ${error.message}`);
